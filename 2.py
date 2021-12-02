c764809eacf99303f6af5ae938125305776b9b7a
ianvol <42355277+ianvol@users.noreply.github.com>
2021-12-02 12:26:09 +0000

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,33 @@
+numflist = []
+numdlist = []
+numulist = []
+
+with open("input.txt", 'r') as f:
+  for line in f:
+    if line.startswith("f"):
+        lnf = line.split()
+        numf = int(lnf[1])
+        numflist.append(numf)
+    elif line.startswith("d"):
+        lnd = line.split()
+        numd = int(lnd[1])
+        numdlist.append(numd)
+    elif line.startswith("u"):
+        lnu = line.split()
+        numu = int(lnu[1])
+        numu = numu * -1
+        numulist.append(numu)
+
+forwardsum = sum(numflist)
+downsum = sum(numdlist)
+upsum = sum(numulist)
+
+verticalsum = upsum + downsum
+
+totalsum = forwardsum * verticalsum
+print("Total sum is: ",totalsum)
+
+        
+
+
+
